c163075dabfb82f91a6e4d5170a84466e8488179
service account <theplan@app-lvn-wcc34.lvn.broadcom.net>
2020-02-13 17:51:23 -0800

    Add parse-csv.js

---
+++ parse-csv.js
@@ -0,0 +1,24 @@
+/* https://github.com/peterthoeny/parse-csv-js, MIT License */
+
+// parse CSV/TSV function
+function parseCsv(data, fieldSep, newLine) {
+    fieldSep = fieldSep || ',';
+    newLine = newLine || '\n';
+    var nSep = '\x1D';
+    var qSep = '\x1E';
+    var nSepRe = new RegExp(nSep, 'g');
+    var qSepRe = new RegExp(qSep, 'g');
+    var fieldRe = new RegExp('(?<=(^|[' + fieldSep + '\\n]))"(|[\\s\\S]+?(?<!"))"(?=($|[' + fieldSep + '\\n]))', 'g');
+    var grid = [];
+    data.replace(/\r/g, '').replace(/\n+$/, '').replace(fieldRe, function(match, p1, p2) {
+        return p2.replace(/\n/g, nSep).replace(/""/g, qSep);
+    }).split(/\n/).forEach(function(line) {
+        var row = line.split(fieldSep).map(function(cell) {
+            return cell.replace(nSepRe, newLine).replace(qSepRe, '"');
+        });
+        grid.push(row);
+    });
+    //console.log('- grid: ' + JSON.stringify(grid, null, ' '));
+    return grid;
+}
+
