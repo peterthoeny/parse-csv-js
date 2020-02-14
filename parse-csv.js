7c9fd2101a8998a74acf24356fcf7811a5adce06
service account <theplan@app-lvn-wcc34.lvn.broadcom.net>
2020-02-13 19:32:04 -0800

    Support commas in quited cells

--- parse-csv.js
+++ parse-csv.js
@@ -6,19 +6,25 @@ function parseCsv(data, fieldSep, newLine) {
     newLine = newLine || '\n';
     var nSep = '\x1D';
     var qSep = '\x1E';
+    var cSep = '\x1F';
     var nSepRe = new RegExp(nSep, 'g');
     var qSepRe = new RegExp(qSep, 'g');
-    var fieldRe = new RegExp('(?<=(^|[' + fieldSep + '\\n]))"(|[\\s\\S]+?(?<!"))"(?=($|[' + fieldSep + '\\n]))', 'g');
+    var cSepRe = new RegExp(cSep, 'g');
+    var fieldRe = new RegExp('(?<=(^|[' + fieldSep + '\\n]))"(|[\\s\\S]+?(?<![^"]"))"(?=($|[' + fieldSep + '\\n]))', 'g');
     var grid = [];
     data.replace(/\r/g, '').replace(/\n+$/, '').replace(fieldRe, function(match, p1, p2) {
-        return p2.replace(/\n/g, nSep).replace(/""/g, qSep);
+        return p2.replace(/\n/g, nSep).replace(/""/g, qSep).replace(/,/g, cSep);
     }).split(/\n/).forEach(function(line) {
         var row = line.split(fieldSep).map(function(cell) {
-            return cell.replace(nSepRe, newLine).replace(qSepRe, '"');
+            return cell.replace(nSepRe, newLine).replace(qSepRe, '"').replace(cSepRe, ',');
         });
         grid.push(row);
     });
     //console.log('- grid: ' + JSON.stringify(grid, null, ' '));
     return grid;
 }
+
+exports.parseCsv = parseCsv;
+
+// EOF
 
