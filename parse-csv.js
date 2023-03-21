9e2b9305e06ddc4b59fa3a647cd6ccb983d89802
service account <theplan@app-lvnhf-wcc-02.lvn.broadcom.net>
2023-03-20 18:23:29 -0700

    Avoid lookbehind for Safari support

--- parse-csv.js
+++ parse-csv.js
@@ -2,29 +2,29 @@
 
 // parse CSV/TSV function
 function parseCsv(data, fieldSep, newLine) {
-    fieldSep = fieldSep || ',';
-    newLine = newLine || '\n';
-    var nSep = '\x1D';
-    var qSep = '\x1E';
-    var cSep = '\x1F';
-    var nSepRe = new RegExp(nSep, 'g');
-    var qSepRe = new RegExp(qSep, 'g');
-    var cSepRe = new RegExp(cSep, 'g');
-    var fieldRe = new RegExp('(?<=(^|[' + fieldSep + '\\n]))"(|[\\s\\S]+?(?<![^"]"))"(?=($|[' + fieldSep + '\\n]))', 'g');
-    var grid = [];
-    data.replace(/\r/g, '').replace(/\n+$/, '').replace(fieldRe, function(match, p1, p2) {
-        return p2.replace(/\n/g, nSep).replace(/""/g, qSep).replace(/,/g, cSep);
-    }).split(/\n/).forEach(function(line) {
-        var row = line.split(fieldSep).map(function(cell) {
-            return cell.replace(nSepRe, newLine).replace(qSepRe, '"').replace(cSepRe, ',');
-        });
-        grid.push(row);
+  fieldSep = fieldSep || ',';
+  newLine = newLine || '\n';
+  const nSep = '\x1D';
+  const nSepRe = new RegExp(nSep, 'g');
+  const qSep = '\x1E';
+  const qSepRe = new RegExp(qSep, 'g');
+  const cSep = '\x1F';
+  const cSepRe = new RegExp(cSep, 'g');
+  const fieldRe = new RegExp('(^|[' + fieldSep + '\\n])"([^"]*(?:""[^"]*)*)"(?=($|[' + fieldSep + '\\n]))', 'g');
+  return data
+    .replace(/\r/g, '')
+    .replace(/\n+$/, '')
+    .replace(fieldRe, (match, p1, p2) => {
+      return p1 + p2.replace(/\n/g, nSep).replace(/""/g, qSep).replace(/,/g, cSep)
+    })
+    .split(/\n/)
+    .map(line => {
+      return line
+        .split(fieldSep)
+        .map(cell => cell.replace(nSepRe, newLine).replace(qSepRe, '"').replace(cSepRe, ','))
     });
-    //console.log('- grid: ' + JSON.stringify(grid, null, ' '));
-    return grid;
 }
 
 exports.parseCsv = parseCsv;
 
 // EOF
-
